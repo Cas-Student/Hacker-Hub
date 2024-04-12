f88277ce2e740552ffe1a130d53cebd42dacb172
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-12 06:17:38 -0400

    Update script.js

--- static/assets/shortcuts/script.js
+++ static/assets/shortcuts/script.js
@@ -2,5 +2,5 @@ const iframe = document.getElementById("ifra");
 
 function loadCoolMathGames() {
   iframe.src="https://www.coolmathgames.com";
-  window.location.href = "./static/go.html";
+  window.location.href = "../go.html";
 }
