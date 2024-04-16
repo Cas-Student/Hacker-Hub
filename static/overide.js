fe99285bd6f5a7c9c7b65afe04f9632e3749675a
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-16 15:30:06 -0400

    Update overide.js

--- static/overide.js
+++ static/overide.js
@@ -1,3 +1,30 @@
-document.onKeyDown(function(e) {
-  alert(e);
+document.onkeydown = overrideKeyboardEvent;
+document.onkeyup = overrideKeyboardEvent;
+var keyIsDown = {};
+
+function overrideKeyboardEvent(e){
+  switch(e.type){
+    case "keydown":
+      if(!keyIsDown[e.keyCode]){
+        keyIsDown[e.keyCode] = true;
+        // do key down stuff here
+      }
+    break;
+    case "keyup":
+      delete(keyIsDown[e.keyCode]);
+      // do key up stuff here
+    break;
+  }
+  disabledEventPropagation(e);
+  e.preventDefault();
+  return false;
+}
+function disabledEventPropagation(e){
+  if(e){
+    if(e.stopPropagation){
+      e.stopPropagation();
+    } else if(window.event){
+      window.event.cancelBubble = true;
+    }
+  }
 }
