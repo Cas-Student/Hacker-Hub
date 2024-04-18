d47fbf566aa26f77c925d0fa5827bf5df09b4e6a
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-18 18:22:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const config = {
   challenge: false, // Set to true if you want to enable password protection.
-  users = {},
+  users: {
+    'Cas':'@tsuMi162!',
+  },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
 }
