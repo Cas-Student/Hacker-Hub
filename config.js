00afa9087f36d5dbfa85469c6682eeba66c48126
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-18 16:34:17 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
+import userData from 'https://grass-grower.github.io/Proxy-Users/Users.js'
+
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: {
-    'Cas': '@stuMi162!',
-  },
+  users = userData.users,
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
 }
