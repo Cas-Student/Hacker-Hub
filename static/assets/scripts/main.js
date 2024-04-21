d958fc6e773c5b4df00174001d5c184ef256cce7
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-21 19:01:35 -0400

    Update main.js

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,8 +1,3 @@
-function Fun() {
-  alert('Trey when he finds out there is a new Proxy...');
-  alert('Trey.png');
-}
-
 document.addEventListener('DOMContentLoaded', function () {
   // Ads
   if (localStorage.getItem('ad') === null || localStorage.getItem('ad') === 'default') {
