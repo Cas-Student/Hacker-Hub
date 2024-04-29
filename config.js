5d90db49d98567bf8a9737d1d694c75340184b30
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-28 20:03:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     '@Override' : '1Q3W5E7R9T2Y4U6I8O0P',
     'Cas' : '@tsuMi162!',
